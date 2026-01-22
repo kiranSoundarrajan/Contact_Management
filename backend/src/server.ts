d32b73010f1b3242846a11b0240d81c944d221c3
kiranSoundarrajan <kiransoundarrajan@example.com>
2026-01-22 10:37:07 +0530

    small changes both frontend and backend server.ts,.env.production-frontend,axiosinstance

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,7 +26,7 @@ const allowedOrigins: string[] = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "http://localhost:5173",
-  process.env.FRONTEND_URL
+  "https://contactmanagement14.netlify.app"
 ].filter(Boolean) as string[];
 
 const corsOptions: CorsOptions = {
